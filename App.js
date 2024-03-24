9de637e37c9cfca1cd81b01c52a39db01ac2ff94
trilok2703 <trilokvallamkonda@gmail.com>
2024-03-24 09:34:33 +0530

    initial boiler template for displaying header, body with restuarant crds and footer components

--- App.js
+++ App.js
@@ -1,9 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () =>{
-    return <h1>Food App 🍔</h1>
+// Header component
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://i.pinimg.com/originals/3c/b2/4f/3cb24f7d0defcb91eb51b091325d5f47.jpg"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+};
+
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+
+// Restaurant card
+const RestaurantCard = () =>{
+    return (
+        <div 
+            className='res-card'
+            style={styleCard}
+        >  
+            <img
+                className='res-logo'
+                alt="res-logo"
+                src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bfcea2108aea7a98f2b370b78b2fdac0'
+            />
+            <h3>Meghana Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>35 minutes</h4>
+        </div>
+    )
+}
+
+// Body
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                {/* Restaurant card */}
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+// Footer
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>@copyright</p>
+        </div>
+    )
+}
+
+const AppLayout = () =>{
+    return (
+        <div className='app'>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
+root.render(<AppLayout/>)
