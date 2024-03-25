5876d40e47d76ffa77ff9aeef0cdaf957ab252ce
trilok2703 <trilokvallamkonda@gmail.com>
2024-03-25 16:10:28 +0530

    Implementing search functionality to filter search results

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,8 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
     const [listOfRestaurants, setListOfRestaurant] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchText,setSearchText] = useState("");
 
     useEffect(()=>{
         fetchData();
@@ -14,30 +16,52 @@ const Body = () => {
     const fetchData = async () => {
         const data = await fetch(RESTAURANT_URL);
         const results = await data.json();
-        console.log(results);
+        // console.log(results);
         setListOfRestaurant(results?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(results?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
     if(listOfRestaurants.length === 0) {
         return <Shimmer/>;
     }
 
+    // console.log(listOfRestaurants)
+
     return (
       <div className="body">
-        <button 
-            className="filter-btn"
-            onClick={()=>{
-                const filteredResults = listOfRestaurants.filter(
-                  (restaurant) => restaurant.info.avgRating > 4.1
-                );
-                setListOfRestaurant(filteredResults);
+        <div className="filter">
+          <input 
+            type="text" 
+            value={searchText}
+            onChange={(e)=>{
+              setSearchText(e.target.value);
             }}
-        >
-            Top Rated Restaurants
-        </button>
+          />
+          {/* Search button for searching restaurants with search text */}
+          <button
+            onClick={()=>{
+             const filteredResults = listOfRestaurants.filter((res)=>res?.info?.name.toLowerCase().includes(searchText.toLowerCase()));
+             setFilteredRestaurants(filteredResults);
+            }}
+          >
+            Search
+          </button>
+          {/* Filter button for top rated restaurant */}
+          <button 
+              className="filter-btn"
+              onClick={()=>{
+                  const filteredResults = listOfRestaurants.filter(
+                    (restaurant) => restaurant.info.avgRating > 4.4
+                  );
+                  setFilteredRestaurants(filteredResults);
+              }}
+          >
+              Top Rated Restaurants
+          </button>
+        </div>
         <div className="res-container">
           {/* Restaurant card */}
-          {listOfRestaurants.map((restaurant) => (
+          {filteredRestaurants.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const [btnName, setBtnName] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -15,6 +19,16 @@ const Header = () => {
                     <li>About us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button 
+                        className="login-btn"
+                        onClick={()=>{
+                            btnName === "Login"
+                              ? setBtnName("Logout")
+                              : setBtnName("Login");
+                        }}
+                    >
+                        {btnName}
+                    </button>
                 </ul>
             </div>
 
