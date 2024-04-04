df731f85aab97048df64fe46ae9614603543247c
trilok2703 <trilokvallamkonda@gmail.com>
2024-04-04 22:22:56 +0530

    Test case for Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,9 @@ const Contact = () =>{
         <div>
             <h2>Contact</h2>
             <p>Please contact us for any issues.</p>
+            <input type="text" placeholder="contact"/>
+            <input type="text" placeholder="me"/>
+            <button>Contact me</button>
         </div>
     )
 }
