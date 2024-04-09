efd8711480fde9308ad18c1948cc24f957019e00
trilok2703 <trilokvallamkonda@gmail.com>
2024-04-09 18:57:21 +0530

    Updates

---
+++ Grocery.bf0d6b9c.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire11e5;(0,e.register)("eLJJh",function(i,r){Object.defineProperty(i.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(i.exports,"default",{get:function(){return n},set:void 0,enumerable:!0,configurable:!0});var l=e("7M0SH"),n=()=>(0,l.jsx)("div",{children:"Grocery vertical for displaying Grocery, which has multiple internal files.."})});
+//# sourceMappingURL=Grocery.bf0d6b9c.js.map
---
+++ Grocery.de6f1644.js
@@ -0,0 +1,2 @@
+var e;(0,(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire11e5).register)("3GGBe",function(i,r){Object.defineProperty(i.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(i.exports,"default",{get:function(){return n},set:void 0,enumerable:!0,configurable:!0});var l=e("2CpG2"),n=()=>(0,l.jsx)("div",{children:"Grocery vertical for displaying Grocery, which has multiple internal files.."})});
+//# sourceMappingURL=Grocery.de6f1644.js.map
